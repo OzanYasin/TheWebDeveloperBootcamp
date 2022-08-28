e4ea803773c5c45013234a041f2f0fe4bb9326e9
Ozan <ozan.yasin97@gmail.com>
2022-08-28 13:29:19 +0300

    JS-Nuggets reduce.js added

--- JS/JS-Nuggets/Reduce.js
+++ JS/JS-Nuggets/Reduce.js
@@ -23,3 +23,79 @@ const dailyTotal = staff.reduce((total, person) => {
 console.log(dailyTotal); // 810
 
 // ------------ Reduce (object example) ------------
+
+// cart example
+
+const cart = [
+  {
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    amount: 1,
+  },
+  {
+    title: 'google pixel ',
+    price: 499.99,
+    amount: 2,
+  },
+  {
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    amount: 4,
+  },
+  {
+    title: 'Xiaomi Redmi Note 5',
+    price: 399.99,
+    amount: 3,
+  },
+];
+
+let { totalItems, cartTotal } = cart.reduce(
+  (total, cartItem) => {
+    // cartItem represent every object in cart array.
+    const { amount, price } = cartItem; // deconstructed amount and price from cart variable.
+    // count items
+    total.totalItems += amount;
+    // count sum
+    total.cartTotal += amount * price;
+    return total;
+  },
+  {
+    totalItems: 0,
+    cartTotal: 0,
+  }
+);
+
+cartTotal = parseFloat(cartTotal.toFixed(2));
+
+console.log(totalItems, cartTotal);
+// console.log(total);
+
+// github repos example
+
+const url = 'https://api.github.com/users/john-smilga/repos?per_page=100';
+
+const fetchRepos = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const newData = data.reduce((total, repo) => {
+    // repo represents every item in 'data' variable
+    const { language } = repo;
+    // if (language) {
+    //   // due to eliminate null
+    //   if (total[language]) {
+    //     total[language] = total[language] + 1;
+    //   } else {
+    //     total[language] = 1;
+    //   }
+    // }
+
+    if (language) {
+      total[language] = total[language] + 1 || 1;
+    }
+
+    return total;
+  }, {});
+  console.log(newData);
+};
+
+fetchRepos();
