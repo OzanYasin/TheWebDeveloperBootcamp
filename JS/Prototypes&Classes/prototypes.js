e2b20bf41b347f2c44505f385a1f23ae8c51c119
Ozan <ozan.yasin97@gmail.com>
2022-09-08 23:16:16 +0300

    Prototype&Classes folder created in JS folder

---
+++ JS/Prototypes&Classes/prototypes.js
@@ -0,0 +1,16 @@
+// Object Oriented Program (OOP)
+
+// Every object in JavaScript has a built-in property, which is called its prototype. The prototype is itself an object.
+// https://www.udemy.com/course/the-web-developer-bootcamp/learn/lecture/23344412#overview
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes#the_prototype_chain
+
+String.prototype.yell = function () {
+  // its a custom prototype that called "yell"
+  console.log(this);
+};
+
+// Object Oriented Programming
+
+// It means organizing our code, designing and structuring our applications by breaking things up into distinct patterns of objects.
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_programming
