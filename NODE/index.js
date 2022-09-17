cb25692d500288136bfd294fd533921ae5f8924a
Ozan <ozan.yasin97@gmail.com>
2022-09-17 16:44:18 +0300

    NODE directory is CREATED

---
+++ NODE/index.js
@@ -0,0 +1,8 @@
+// https://nodejs.org/en/
+// What is Node?
+// Technically it's a JavaScript runtime.
+// Javascript runtime refers to where your javascript code is executed when you run it. That said, javascript can be executed on google chrome, in which case your javascript runtime is v8
+
+// So it's the job of a browser to follow the rules and implement the language.
+
+// !! Node is an implementation of JavaScript that runs outside of the browser.
