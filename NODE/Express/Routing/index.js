3942698eabde7c4622847ef9dfa8c7a4ca0ccbd3
Ozan <ozan.yasin97@gmail.com>
2022-09-19 18:06:46 +0300

    NODE/Express updated

--- NODE/Express/Routing/index.js
+++ NODE/Express/Routing/index.js
@@ -23,18 +23,13 @@ app.get('/cats', (req, res) => {
   res.send('MEOW!');
 });
 
+app.post('/cats', (req, res) => {
+  // console.log('Cat request!');
+  res.send('THIS IS A POST!');
+});
+
 app.get('/dogs', (req, res) => {
   res.send('WOOF!');
-});
-
-app.get('*', (req, res) => {
-  // '*' means everything
-  // Routes are matched in order. So, if * goes t o first on the line, it will cover every path. This is what we do not want it.
-  res.send('I dont know that path.');
-});
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
 });
 
 // !! Remember,
@@ -42,3 +37,26 @@ app.listen(3000, () => {
 // Response is an object made by express, both of which are passed into this callback and response has.
 
 // --------------------------------------------
+
+// ** Working with Query Strings **
+
+app.get('/search', (req, res) => {
+  // console.log(req.query);
+  const { q, color } = req.query;
+  if (!q) {
+    res.send('Nothing found if nothing searched.');
+  } else if (!color) {
+    res.send(`Search results for ${q}. <hr> There is no color to set.`);
+  }
+  res.send(`Search results for ${q}. <hr> Color is ${color}`);
+});
+
+app.get('*', (req, res) => {
+  // '*' means everything
+  // Routes are matched in order. So, if * goes t o first on the line, it will cover every path. This is what we do not want it.
+  res.send('I do not know that path.');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
