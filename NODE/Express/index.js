24601afbaf41ca86d4501a84417e11d06f037bcb
Ozan <ozan.yasin97@gmail.com>
2022-09-19 12:53:01 +0300

    NODE/Express updated

--- NODE/Express/index.js
+++ NODE/Express/index.js
@@ -16,10 +16,22 @@ const express = require('express');
 const app = express();
 // console.dir(app);
 
-app.use(() => {
+app.use((req, res) => {
   // .use means anytime we have an incoming request, this callback will run.
-  console.log('We Got A New Request!');
+
+  // req and res parameters are objects that made by express (built-in).
+
+  // !! It passes in two objects a request object based upon the incoming request, the HTTP request.
+  // And response object, that is going to be used to generate a response that will be eventually sent back to whoever requested it.
+
+  console.log('We got a new request!');
+  // res.send('We got your Request. This is a response!'); // content-type: text/html
+  res.send({ color: 'red' }); // content-type: application/json
+  // res.send is going to actually send and generate a HTTP response and this is all express is doing for us.
 });
+
+// !! HTTP Request is not a JavaScript object, its text information.
+// Express take that data, it parses it and it turns it into a object that it passes in.
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
